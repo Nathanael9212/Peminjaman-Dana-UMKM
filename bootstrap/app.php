c378c23782c245f279b1420f470443701358dad0
Nathanael Kristian Sujarwo <nathanaelkristian0902@gmail.com>
2025-12-17 21:50:45 +0700

    Trust Railway proxy for HTTPS

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->alias([
             'admin' => \App\Http\Middleware\AdminMiddleware::class,
         ]);
-        //
+        
+        // Trust Railway proxy for HTTPS
+        $middleware->trustProxies(at: '*');
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
