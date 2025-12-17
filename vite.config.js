0fe2f3d0050f7ed5934f3ab265bfd8c50e14f5b4
Nathanael Kristian Sujarwo <nathanaelkristian0902@gmail.com>
2025-12-17 20:40:57 +0700

    update

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css', // default
-                'resources/css/home.css', // tambahkan ini
+                'resources/css/app.css',
                 'resources/js/app.js',
             ],
             refresh: true,
