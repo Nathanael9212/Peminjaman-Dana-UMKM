1307d0758a208eaaa9b54da6d6df4b1806d04328
Nathanael Kristian Sujarwo <nathanaelkristian0902@gmail.com>
2025-12-17 20:49:53 +0700

    Fix view binding error for Vercel

--- api/index.php
+++ api/index.php
@@ -1,3 +1,10 @@
 <?php
 
+// Clear bootstrap cache in Vercel serverless environment
+$bootstrapCache = __DIR__ . '/../bootstrap/cache';
+if (is_dir($bootstrapCache)) {
+    array_map('unlink', glob("$bootstrapCache/*.php"));
+}
+
+// Forward to Laravel
 require __DIR__ . '/../public/index.php';
